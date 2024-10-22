02e9c2cc7d846b2dea4987c27cff2bbf9001cb2b
islamu <114508095+islamu@users.noreply.github.com>
2024-10-23 01:45:34 +0200

    Update algoXOR.h

--- chiffrement/algoXOR.h
+++ chiffrement/algoXOR.h
@@ -1,21 +1,4 @@
-<<<<<<< HEAD
-// algoXOR.h
 
-
-#ifndef algoXOR_H
-#define algoXOR_H
-
-#include <stddef.h>
-
-// Fonction pour chiffrer ou déchiffrer un message avec XOR
-// message : pointeur vers le message en clair ou chiffré
-// cle : pointeur vers la clé de chiffrement
-// res : pointeur vers le buffer de sortie (doit être alloué par l'appelant)
-// len : longueur du message en octets
-void algoXOR(const unsigned char *message, const unsigned char *cle, unsigned char *res, size_t len);
-
-#endif // algoXOR_H
-=======
 // algoXOR.h
 
 #ifndef ALGOXOR_H
@@ -31,4 +14,4 @@ void algoXOR(const unsigned char *message, const unsigned char *cle, unsigned ch
 void algoXOR(const unsigned char *message, const unsigned char *cle, unsigned char *output, size_t len);
 
 #endif // ALGOXOR_H
->>>>>>> 3a01e5abc3530918ce7b568bb1bd49f5a73e5798
+
