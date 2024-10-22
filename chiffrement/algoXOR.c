52b45061a9a3eeab8469036cca082c0acea0aa70
islamu <114508095+islamu@users.noreply.github.com>
2024-10-23 01:44:40 +0200

    Update algoXOR.c

--- chiffrement/algoXOR.c
+++ chiffrement/algoXOR.c
@@ -1,12 +1,9 @@
 
 #include "algoXOR.h"
 #include <stddef.h>
-<<<<<<< HEAD
 
-=======
->>>>>>> 3a01e5abc3530918ce7b568bb1bd49f5a73e5798
 // Fonction pour chiffrer ou déchiffrer un message avec XOR
-void algoXOR(const unsigned char *message, const unsigned char *cle, unsigned char *output, size_t len) {
+void algoXOR(const unsigned char *message, const unsigned char *cle, unsigned char *res, size_t len) {
     size_t cle_len = 0;
 
     // Calculer la longueur de la clé
@@ -15,6 +12,6 @@ void algoXOR(const unsigned char *message, const unsigned char *cle, unsigned ch
     }
 
     for (size_t i = 0; i < len; i++) {
-        output[i] = message[i] ^ cle[i % cle_len];
+        res[i] = message[i] ^ cle[i % cle_len];
     }
 }
